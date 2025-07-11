3ad34c419a5730d55acf3a337c99873782c24def
wolfmanjacko <dostie@telus.net>
2025-07-11 11:59:24 -0700

    added time

--- pages/test.js
+++ pages/test.js
@@ -1,11 +1,18 @@
 export async function getServerSideProps() {
+  console.log(">>> getServerSideProps is running on the server <<<");
   return {
     props: {
       message: 'This is rendered server-side at request time',
+      time: new Date().toISOString(),
     },
   };
 }
 
 export default function Test({ message }) {
-  return <div>{message}</div>;
+   return (
+    <div>
+      <p>{message}</p>
+      <p>{time}</p>
+    </div>
+  );
 }
